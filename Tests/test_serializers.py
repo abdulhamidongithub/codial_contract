c0392ed69bd97240bcd8c1594c6649009e8e98fd
abdulhamidongithub <1997abdulhamid@gmail.com>
2021-12-26 17:15:35 +0300

    Started writing tests

---
+++ Tests/test_serializers.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from shartnoma.serializers import *
+
+class TestKursSerializer(TestCase):
+    def setUp(self) -> None:
+        pass
+    def test_data(self):
+        assert True
+
+
+
