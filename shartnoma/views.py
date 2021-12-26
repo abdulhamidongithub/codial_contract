10e5ff36fe4637450aaee20be04667508723b9f4
abdulhamidongithub <1997abdulhamid@gmail.com>
2021-12-26 11:15:19 +0300

    Added search, ordering filters

--- shartnoma/views.py
+++ shartnoma/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 from shartnoma.models import Student
 from .serializers import *
@@ -7,8 +7,9 @@ from .serializers import *
 class StudentViewSet(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    filter_backends = [SearchFilter,]
+    filter_backends = [SearchFilter, OrderingFilter,]
     search_fields = ["ism", "id", "familiya", "jins", "tel1", "tel2"]
+    ordering_fields = ["id", "jins","ism",]
 class UstozViewSet(ModelViewSet):
     queryset = Ustoz.objects.all()
     serializer_class = UstozSerializer
